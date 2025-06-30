5278ae729d184265940f454a94280307ab09210e
tristanm71 <134559647+tristanm71@users.noreply.github.com>
2025-06-30 12:27:50 -0500

    Create .env file and create flask key

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, redirect
 from dotenv import load_dotenv
+import os 
+
+load_dotenv()
 
 app = Flask(__name__)
-
+app.config["SECRET_KEY"] = os.environ.get("FLASK_KEY")
 
 @app.route("/")
 def home():
