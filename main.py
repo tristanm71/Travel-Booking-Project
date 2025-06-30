62c79d048f2c4252e16d17821d4a4d8040203336
tristanm71 <134559647+tristanm71@users.noreply.github.com>
2025-06-30 13:41:03 -0500

    Load bootstrap5 in main, and header file, placed a navbar

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, redirect
 from dotenv import load_dotenv
 import os 
+from flask_bootstrap import Bootstrap5
 
 load_dotenv()
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("FLASK_KEY")
+
+bootstrap = Bootstrap5(app)
 
 @app.route("/")
 def home():
